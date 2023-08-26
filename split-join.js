7432f9d7c7dd41e9066b6a1f5aaccd74c03afa1b
Abdullah Al Marjuc Talha <abdullahalmarjuct@gmail.com>
2023-08-27 02:15:24 +0600

    split-join added

---
+++ split-join.js
@@ -0,0 +1,25 @@
+const lyrics = 'Tumi bondhu kala pakhi ami jeno ki. bosonto kale tomai bolte pari ni.';
+const parts = lyrics.split(' ');
+const sentences = lyrics.split('.');
+const chars = lyrics.split('');
+console.log(parts);
+console.log(sentences);
+console.log(chars);
+
+// --------------------------
+
+const partial = lyrics.slice(5,8);
+console.log(partial);
+
+const partial2 = lyrics.substring(5,8);
+console.log(partial2);
+
+
+const lines = [
+    'Tumi bondhu kala pakhi ami jeno ki',
+    ' bosonto kale tomai bolte pari ni',
+    ''
+  ]
+
+  const newSong = lines.join('. ');
+  console.log(newSong)
